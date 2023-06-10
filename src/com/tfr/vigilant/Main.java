39f71d36cd0d146cb689777a269b2848a7569c2d
Erik Hage <ehage4@gmail.com>
2023-06-10 09:47:14 -0400

    package structure

--- src/com/tfr/vigilant/Main.java
+++ src/com/tfr/vigilant/Main.java
@@ -1,3 +1,5 @@
+package com.tfr.vigilant;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/com/tfr/vigilant/controller/MessageController.java
@@ -0,0 +1,4 @@
+package com.tfr.vigilant.controller;
+
+public class MessageController {
+}
