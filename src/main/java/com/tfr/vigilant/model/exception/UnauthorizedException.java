7960bdb3352b57cbaa4354aa8d4003954931c844
Erik Hage <ehage4@gmail.com>
2024-02-27 17:26:00 -0500

    fix compilation error

--- src/main/java/com/tfr/vigilant/model/exception/UnauthorizedException.java
+++ src/main/java/com/tfr/vigilant/model/exception/UnauthorizedException.java
@@ -1,9 +1,7 @@
 package com.tfr.vigilant.model.exception;
 
 public class UnauthorizedException extends Exception {
-    private final String message;
 
     public UnauthorizedException() {
-        this.message = message;
     }
 }
